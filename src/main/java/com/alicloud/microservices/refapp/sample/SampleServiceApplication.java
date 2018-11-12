2514c2c3c6d79a8b72c8c21facaa715f4efdd445
lifengchuan <lifengchuan@neucloud.cn>
2018-11-12 16:37:49 +0800

    update  pom build

--- src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
+++ src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
@@ -1,24 +1,22 @@
 package com.alicloud.microservices.refapp.sample;
 
-import java.net.MalformedURLException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.MalformedURLException;
+import java.util.List;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-@EnableHystrix
 public class SampleServiceApplication {
 
 	@Autowired
