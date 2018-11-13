0628e890cc3c326fa0dbf9dab9ee80e196f8b092
lifengchuan <lifengchuan@neucloud.cn>
2018-11-13 11:12:43 +0800

    update

--- src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
+++ src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-@EnableEurekaClient
 public class SampleServiceApplication {
 
     @Autowired
