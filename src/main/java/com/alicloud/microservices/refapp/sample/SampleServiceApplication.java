1f857936f9ad302a30b484c5d60d96b37876d46b
lifengchuan <lifengchuan@neucloud.cn>
2018-11-14 16:46:25 +0800

    update

--- src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
+++ src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
@@ -1,11 +1,18 @@
 package com.alicloud.microservices.refapp.sample;
 
+import com.alibaba.fastjson.JSON;
+import io.fabric8.kubernetes.api.model.Endpoints;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient;
+import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,6 +58,12 @@ public class SampleServiceApplication {
     public String hello(@RequestParam(value = "service", required = false) String serviceName2) {
         String services = discoveryClient.getServices().stream().collect(Collectors.joining(","));
 
+        CompositeDiscoveryClient cd = (CompositeDiscoveryClient) discoveryClient;
+
+        for (DiscoveryClient d : cd.getDiscoveryClients()) {
+            System.out.println("DiscoveryClient: " + d.description());
+
+        }
 
 
 
