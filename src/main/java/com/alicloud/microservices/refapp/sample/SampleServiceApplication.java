d3d4b90734c9e7f5810ea8ea4539aa431eed9386
lifengchuan <lifengchuan@neucloud.cn>
2018-11-14 16:05:39 +0800

    update

--- src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
+++ src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
@@ -1,18 +1,11 @@
 package com.alicloud.microservices.refapp.sample;
 
-import com.alibaba.fastjson.JSON;
-import io.fabric8.kubernetes.api.model.Endpoints;
-import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient;
-import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,10 +26,6 @@ public class SampleServiceApplication {
 
     @Autowired
     private DiscoveryClient discoveryClient;
-    @Autowired
-    private KubernetesClient client;
-    @Autowired
-    private KubernetesDiscoveryProperties properties;
 
 
     public static void main(String[] args) {
@@ -62,28 +51,7 @@ public class SampleServiceApplication {
     public String hello(@RequestParam(value = "service", required = false) String serviceName2) {
         String services = discoveryClient.getServices().stream().collect(Collectors.joining(","));
 
-        CompositeDiscoveryClient cd = (CompositeDiscoveryClient) discoveryClient;
 
-        for (DiscoveryClient d : cd.getDiscoveryClients()) {
-            System.out.println("DiscoveryClient: " + d.description());
-
-        }
-
-        String serviceName = properties.getServiceName();
-        String podName = System.getenv(HOSTNAME);
-        ServiceInstance defaultInstance = new DefaultServiceInstance(serviceName, "localhost", 8080, false);
-
-        Endpoints endpoints = client.endpoints().withName(serviceName).get();
-
-        System.out.println(JSON.toJSONString(endpoints));
-
-        Service service = client.services().withName(serviceName).get();
-
-        System.out.println("service: " + service);
-
-        if (service != null) {
-            System.out.println("service: " + JSON.toJSONString(service));
-        }
 
 
         return "Hello! This is from " + "! ," + services + " ,client: " + discoveryClient.description();
